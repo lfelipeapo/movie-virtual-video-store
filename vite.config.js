fad858683c6274590c15f5e1433b996aed3f9baf
Luiz Felipe Apolinário <lfelipeapo@hotmail.com>
2025-06-28 23:36:32 -0300

    chore: base configs

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/movie-virtual-video-store/',
   plugins: [
     vue(),
     vueDevTools(),
